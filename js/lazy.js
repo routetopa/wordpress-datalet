54d5c8feb3e52535fd82c5a4073596568c6bbf19
Luca Vicidomini <lvicido@libero.it>
2019-02-22 13:10:59 +0100

    Lazy loading compatibility and polyfill

--- js/lazy.js
+++ js/lazy.js
@@ -1,21 +1,24 @@
-$('.rtpa-lazy.rtpa-hide').click(function() {
-    if ($(this).hasClass('rtpa-hide')) {
-        var datalet = $(decodeURIComponent($(this).data('datalet')));
-        $(this)
-            .removeClass('rtpa-hide')
-            .removeData('datalet')
-            .html(datalet);
-    }
-});
-
-$(document).scroll(function() {
-    var y = $(this).scrollTop() + $(window).height();
-
-    $('.rtpa-lazy.rtpa-hide').each(function() {
-        if ( $(this).position().top <= y + 100 ) {
-            $(this).click();
+jQuery(document).ready(function ($) {
+    $('.rtpa-lazy.rtpa-hide').click(function() {
+        if ($(this).hasClass('rtpa-hide')) {
+            var datalet = $(decodeURIComponent($(this).data('datalet')));
+            $(this)
+                .removeClass('rtpa-hide')
+                .removeData('datalet')
+                .html(datalet);
+            HTMLImports.loadImports(document.body)
         }
     });
-});
 
-$(document).scroll();
+    $(document).scroll(function() {
+        var y = $(this).scrollTop() + $(window).height();
+
+        $('.rtpa-lazy.rtpa-hide').each(function() {
+            if ( $(this).position().top <= y + 100 ) {
+                $(this).click();
+            }
+        });
+    });
+
+    $(document).scroll();
+})
